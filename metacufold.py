e1f8f43f2e8b54fce21c2be05b87d150e8b50024
Kenan Schaefkofer <kbitikofer@gmail.com>
2023-03-06 18:14:27 -0800

    Get file structure set up

---
+++ metacufold.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+from config import Config as C;
+
+import sys
+import os
+import argparse
+
+def sync():
+    print("Syncing...")
+
+def bet_once():
+    print("bet_once...")
+    print("API Key: " + C.API_KEY)
+
+def help():
+    print("help...")
+    print("API Key: " + C.API_KEY)
+
+# For the main method, check arguments to see function to run
+if __name__ == "__main__":
+
+    # Create the cache directory if it doesn't exist
+    if not os.path.exists("cache"):
+        os.makedirs("cache")
+
+    # Create the logs directory if it doesn't exist
+    if not os.path.exists("logs"):
+        os.makedirs("logs")
+
+    # Check the first argument
+    if len(sys.argv) > 1:
+        # Check the first argument
+        if sys.argv[1] == "sync":
+            # Run the fold method
+            sync()
+            sys.exit(0)
+        elif sys.argv[1] == "bet-once":
+            # Run the unfold method
+            bet_once()
+            sys.exit(0)
+        elif sys.argv[1] == "help":
+            # Print the help message
+            help()
+            sys.exit(0)
+        else:
+            print("Invalid argument: " + sys.argv[1])
+            print()
+
+    # If no valid arguments were given, print the help message
+    help()
+    sys.exit(1)
